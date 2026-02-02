a53f6389c57615eb9f7a57b92589230be7f567a0
Shiwantha-I-Rodrigo <shiwantha.indunil@gmail.com>
2026-02-02 15:48:55 +0100

    add_config

---
+++ KingGardenViewHotel/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$_HOTEL_LOCATION = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126749.03037170188!2d80.80338665850445!3d6.9017009311803275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae479436af5e1c9%3A0x260d86654891194f!2sKing%20Garden%20View%20Hotel!5e0!3m2!1sen!2sit!4v1710145864576!5m2!1sen!2sit";
+$_CUSTOMER_VERIFICATION_LINK = "http://localhost/bittest/verify.php";
+
+$_DB_SERVER = "localhost";
+$_DB_USERNAME = "root";
+$_DB_PASSWORD = "XXX";
+$_DB_NAME = "kgvh";
+
+$_EMAIL_ADDRESS = "someone@somewhere.com";
+$_EMAIL_PASSWORD = "XXXXXXXXXXXXXXXXX";
+
+define('SYSTEM_BASE_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/system/');
+define('WEB_BASE_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/web/');
+define('BASE_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/');
